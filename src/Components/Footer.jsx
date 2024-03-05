4c2fc6db884bec5de2e40623a6eb20855380c59d
unknown <divyaneela75@gmail.com>
2024-03-05 17:27:37 +0530

    first commit

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,24 @@
+
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <div>
+          <span>Logo</span>
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Incidunt delectus consequuntur suscipit ex dicta omnis, facere nostrum, asperiores dignissimos eaque molestiae laborum ut repudiandae doloribus rem unde quas? Perspiciatis, eligendi!</p>
+        </div>
+        <div>
+          <h2></h2>
+          <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+          </ul>
+        </div>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
