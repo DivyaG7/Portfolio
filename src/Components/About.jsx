04fb68a8704b37045216a184f879f1ca90e02353
unknown <divyaneela75@gmail.com>
2024-03-06 11:38:04 +0530

    first commit

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -19,9 +19,8 @@ const About = () => {
       <img data-aos="fade-down" src={img2} width={290} height={290} className='rounded border-2 p-1 border-fuchsia-500 img_glow' alt='' />
       <div className="h-full lg:py-40 flex flex-col justify-center lg:items-start items-center text-white">
         <h1 data-aos="fade-right" className="text-[52px] font-semibold mb-8 leading-normal text-fuchsia-500 uppercase">About me</h1>
-        <p data-aos="fade-left">I am driven by my love for coding and my enthusiasm for staying at the forefront of web development trends. With a focus on continuous learning, I am always eager to expand my skill set and explore new technologies. My goal is to contribute to projects that make a meaningful impact and push the boundaries of what is possible in the digital realm.
-
-</p>
+        {/* <p data-aos="fade-left">I am driven by my love for coding and my enthusiasm for staying at the forefront of web development trends. With a focus on continuous learning, I am always eager to expand my skill set and explore new technologies. My goal is to contribute to projects that make a meaningful impact and push the boundaries of what is possible in the digital realm.</p> */}
+        <p data-aos="fade-left">I thrive on challenges and constantly seek to expand my knowledge and skill set in the ever-evolving field of web development. Whether it's building sleek and intuitive user interfaces or optimizing backend processes for maximum efficiency, I'm always up for the task. With a commitment to staying abreast of the latest technologies and trends, I strive to deliver innovative solutions that push the boundaries of what's possible on the web. Let's collaborate and bring your digital vision to life!</p>
         <div className="flex mt-8 gap-2">
           <div className="flex items-center justify-center">
             <div className="flex space-x-2">
